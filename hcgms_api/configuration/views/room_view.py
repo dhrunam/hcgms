23be5a3ea6d7c6e912a9bc73b72beded00f5dd0a
Dhrubajyoti Borah <dhrubajyotiborah@Dhrubajyotis-MacBook-Pro.local>
2023-02-24 14:16:22 +0530

    Masters are developed

--- hcgms_api/configuration/views/room_view.py
+++ hcgms_api/configuration/views/room_view.py
@@ -11,6 +11,20 @@ class RoomList(generics.ListCreateAPIView):
     queryset = models.Room.objects.all()
     serializer_class = serializers.RoomSerializer
     # pagination.PageNumberPagination.page_size = 100
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases item  received
+        for the specified order .
+        """
+
+        # order_number = self.request.data['order_no']
+        search_text = self.request.query_params.get('property')
+
+        if(search_text):
+            return models.Room.objects.filter(property=search_text)
+        else:
+            return models.Room.objects.all()
+
 
 
 class RoomDetails(generics.RetrieveUpdateDestroyAPIView):
