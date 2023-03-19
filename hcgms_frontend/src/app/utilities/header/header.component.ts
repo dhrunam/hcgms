c31d7a2d163aaaf1248023a4d1cc38160a834fa1
Raja Siddharth Raju <comrade@Rajas-MacBook-Pro.local>
2023-03-19 21:16:28 +0530

    Checkin

--- hcgms_frontend/src/app/utilities/header/header.component.ts
+++ hcgms_frontend/src/app/utilities/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { AuthService } from 'src/app/services/auth-service/auth-service';
 import { LocalStorageService } from 'src/app/services/local-storage-service/local-storage.service';
 
@@ -9,11 +9,30 @@ import { LocalStorageService } from 'src/app/services/local-storage-service/loca
 })
 export class HeaderComponent {
   username:string = '';
+  localStorage: any;
+  screenWidth:any;
+  screenHeight:any;
+  @Output() onToggle = new EventEmitter<boolean>();
+  @Input() toggleValue: boolean = false;
   constructor(private authService: AuthService, private localStorageService: LocalStorageService){
     this.username = this.localStorageService.getUserName();
   }
-
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {  
+    this.screenWidth = window.innerWidth;  
+    this.screenHeight = window.innerHeight;
+    if(this.screenWidth > 1136 || this.screenWidth < 1136){
+      this.toggleValue = false;
+    }
+    this.onToggle.emit(this.toggleValue);
+  }
+  onInitToggle(){
+    this.toggleValue = !this.toggleValue;
+    this.onToggle.emit(this.toggleValue);
+  }
   onLogout(){
-    this.authService.logout();
+    this.authService.logout().subscribe({
+      next: () => window.location.href = '/',
+    });
   }
 }
